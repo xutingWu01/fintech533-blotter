fc690511b2ef8450751ef81a5be7376f50206020
xw218 <xw218@duke.edu>
2023-02-23 12:38:49 -0500

    added files

---
+++ app.py
@@ -0,0 +1,228 @@
+from dash import Dash, html, dcc, dash_table, Input, Output, State
+from datetime import datetime
+import plotly.express as px
+import os
+import base64
+import dash_bootstrap_components as dbc
+import helper
+
+
+# app = Dash(__name__)
+app = Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
+# takes python and generate html
+controls = dbc.Card(
+    [
+        html.Div(
+            [
+                dbc.Label("Asset"),
+                dbc.Input(id='asset-id', type='text', value="IVV"),
+                html.Br(),
+            ]
+        ),
+        # date picker
+        html.Div(
+            [
+                html.H5("Please enter start date and end date for the data which you want to query"),
+                dcc.DatePickerRange(
+                    id='my-date-picker-range',
+                    min_date_allowed=datetime(2023, 1, 30),
+                    max_date_allowed=datetime.now(),
+                ),
+                # html.Br(),
+                dbc.Alert(id='query_date_div', is_open=False,),
+            ]
+        ),
+        html.Div(
+            [
+                html.Hr(),
+                dbc.Row(dbc.Button('Query', color="primary", id='query', n_clicks=0, className="mb-3",)),
+            ]
+        )
+    ],
+    body=True,
+)
+
+raw_data_table = html.Div([
+    html.H2('RAW DATA'),
+    dash_table.DataTable(
+            id="raw-data",
+            page_action='none',
+            style_table={'height': '300px', 'overflowY': 'auto'}
+        ),
+])
+
+paramater_table = dbc.Card(
+    [
+        html.Div(
+            [
+                dbc.Label("alpha1"),
+                dbc.Input(id='alpha', type='number', value="-0.01"),
+            ]
+        ),
+        html.Div(
+            [
+                dbc.Label("n1"),
+                dbc.Input(id='n', type='number', value="3"),
+            ]
+        ),
+        html.Div(
+            [
+                dbc.Label("alpha2"),
+                dbc.Input(id='alpha2', type='number', value="0.01"),
+            ]
+        ),
+        html.Div(
+            [
+                dbc.Label("n2"),
+                dbc.Input(id='n2', type='number', value="5"),
+            ]
+        ),
+        html.Div(
+            [
+                html.Hr(),
+                dbc.Row(dbc.Button('Submit', color="primary", id='submit', n_clicks=0, className="mb-3",)),
+            ]
+        )
+    ],
+    body=True,
+)
+
+entry_table = html.Div([
+    html.H2('ENTRY-BLOTTER'),
+    dash_table.DataTable(
+        id="entry-blotter-tbl",
+        page_action='none',
+        style_table={'height': '300px', 'overflowY': 'auto'}
+    ),
+])
+
+exit_table = html.Div([
+    html.H2('EXIT-BLOTTER'),
+    dash_table.DataTable(
+        id="exit-blotter-tbl",
+        page_action='none',
+        style_table={'height': '300px', 'overflowY': 'auto'}
+    ),
+])
+
+tabs = html.Div([
+    html.H2("Blotter"),
+    dbc.Tabs(
+            [
+                dbc.Tab(label="Entry", tab_id="Entry"),
+                dbc.Tab(label="Exit", tab_id="Exit"),
+            ],
+            id="tabs",
+            active_tab="Entry",
+        ),
+    html.Div(id="tab-content", className="p-4"),
+])
+
+image_filename = 'fig.png'
+encoded_image = base64.b64encode(open(image_filename, 'rb').read())
+
+app.layout = dbc.Container(
+    [
+        dcc.Store(id="entry"),
+        dcc.Store(id="exit"),
+    html.Div([
+        html.H1("Blotter for entry and exit blotter"),
+        html.Hr(),
+        dbc.Row(
+            [
+                dbc.Col(controls, md=4),
+                dbc.Col(raw_data_table, md=8),
+            ],
+            align="center",
+        ),
+        dbc.Row(
+            [
+                dbc.Col(paramater_table, md=4),
+                dbc.Col(tabs, md=8),
+            ],
+            align="center",
+        ),
+        # dbc.Row(entry_table, md=8),
+        # dbc.Row(exit_table, md=12),
+    ]),
+    html.Img(src='data:image/png;base64,{}'.format(encoded_image.decode()), style={'height':'50%', 'width':'50%'}),
+    ],
+    fluid=True,
+)
+
+# Whenever an input property changes, the function that the callback decorator wraps will get called automatically
+# query data from refinitiv
+@app.callback(
+    Output("raw-data", "data"),
+    Output("query_date_div", "children"),
+    Output("query_date_div", "is_open"),
+    Input("query", "n_clicks"),
+    [State('asset-id', 'value'), State('my-date-picker-range', 'start_date'),
+     State('my-date-picker-range', 'end_date'), State("query_date_div", "is_open")],
+    prevent_initial_call=True
+)
+def query_refinitiv(n_clicks, asset_id, start_date, end_date, is_open):
+    if start_date is None or end_date is None:
+        return [], "Please enter start and end date", True
+    # get start dates and end dates
+    start_date_object = datetime.fromisoformat(start_date)
+    start_date_string = start_date_object.strftime("%Y-%m-%d")
+    end_date_object = datetime.fromisoformat(end_date)
+    end_date_string = end_date_object.strftime("%Y-%m-%d")
+
+    global ivv_prc
+    ivv_prc = helper.query_date(start_date_string, end_date_string, asset_id)
+    return ivv_prc.to_dict('records'), None, False
+
+
+
+# generate blotter
+@app.callback(
+    Output("entry", "data"),
+    Output("exit", "data"),
+    Input("submit", "n_clicks"),
+    [State("alpha", "value"), State("n", "value"), State("alpha2", "value"), State("n2", "value")],
+    prevent_initial_call=True
+)
+def render_blotter(n_clicks, alpha1, n1, alpha2, n2):
+    print("rendering result")
+    entry_orders, exit_orders = helper.generateOrders(float(alpha1), int(n1), float(alpha2), int(n2), ivv_prc)
+    return entry_orders.to_dict('records'), exit_orders.to_dict('records')
+
+
+@app.callback(
+    Output("tab-content", "children"),
+    [Input("tabs", "active_tab"), Input("entry", "data"), Input("exit", "data")],
+)
+def render_tab_content(active_tab, entry, exit):
+    """
+    This callback takes the 'active_tab' property as input, as well as the
+    stored graphs, and renders the tab content depending on what the value of
+    'active_tab' is.
+    """
+    if active_tab and entry and exit is not None:
+        if active_tab == "Entry":
+            return html.Div([
+                html.H2('ENTRY-BLOTTER'),
+                dash_table.DataTable(
+                    data = entry,
+                    id="entry-blotter-tbl",
+                    page_action='none',
+                    style_table={'height': '300px', 'overflowY': 'auto'}
+                ),
+            ])
+        elif active_tab == "Exit":
+            return html.Div([
+                html.H2('EXIT-BLOTTER'),
+                dash_table.DataTable(
+                    data = exit,
+                    id="exit-blotter-tbl",
+                    page_action='none',
+                    style_table={'height': '300px', 'overflowY': 'auto'}
+                ),
+            ])
+    return "No tab selected"
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
+    # serve(app)
---
+++ helper.py
@@ -0,0 +1,215 @@
+import pandas as pd
+from datetime import datetime
+import numpy as np
+import os
+import eikon as ek
+import refinitiv.data as rd
+
+#####################################################
+def query_date(start_date_str, end_date_str, asset):
+    ek.set_app_key(os.getenv('EIKON_API'))
+
+    # start_date_str = '2023-01-30'
+    # end_date_str = '2023-02-08'
+
+    ivv_prc, ivv_prc_err = ek.get_data(
+        instruments = [asset],
+        fields = [
+            'TR.OPENPRICE(Adjusted=0)',
+            'TR.HIGHPRICE(Adjusted=0)',
+            'TR.LOWPRICE(Adjusted=0)',
+            'TR.CLOSEPRICE(Adjusted=0)',
+            'TR.PriceCloseDate'
+        ],
+        parameters = {
+            'SDate': start_date_str,
+            'EDate': end_date_str,
+            'Frq': 'D'
+        }
+    )
+
+    ivv_prc['Date'] = pd.to_datetime(ivv_prc['Date']).dt.date
+    ivv_prc.drop(columns='Instrument', inplace=True)
+    return ivv_prc
+
+def get_next_business_day(ivv_prc):
+    ##### Get the next business day from Refinitiv!!!!!!!
+    rd.open_session()
+
+    next_business_day = rd.dates_and_calendars.add_periods(
+        start_date= ivv_prc['Date'].iloc[-1].strftime("%Y-%m-%d"),
+        period="1D",
+        calendars=["USA"],
+        date_moving_convention="NextBusinessDay",
+    )
+
+    rd.close_session()
+
+    return next_business_day
+######################################################
+
+# # Parameters:
+# alpha1 = -0.01
+# n1 = 3
+#
+def generateOrders(alpha1, n1, alpha2, n2, ivv_prc):
+    next_business_day = get_next_business_day(ivv_prc)
+    # submitted entry orders
+    submitted_entry_orders = pd.DataFrame({
+        "trade_id": range(1, ivv_prc.shape[0]),
+        "date": list(pd.to_datetime(ivv_prc["Date"].iloc[1:]).dt.date),
+        "asset": "IVV",
+        "trip": 'ENTER',
+        "action": "BUY",
+        "type": "LMT",
+        "price": round(
+            ivv_prc['Close Price'].iloc[:-1] * (1 + alpha1),
+            2
+        ),
+        'status': 'SUBMITTED'
+    })
+
+    # if the lowest traded price is still higher than the price you bid, then the
+    # order never filled and was cancelled.
+    with np.errstate(invalid='ignore'):
+        cancelled_entry_orders = submitted_entry_orders[
+            np.greater(
+                ivv_prc['Low Price'].iloc[1:][::-1].rolling(3).min()[::-1].to_numpy(),
+                submitted_entry_orders['price'].to_numpy()
+            )
+        ].copy()
+    cancelled_entry_orders.reset_index(drop=True, inplace=True)
+    cancelled_entry_orders['status'] = 'CANCELLED'
+    cancelled_entry_orders['date'] = pd.DataFrame(
+        {'cancel_date': submitted_entry_orders['date'].iloc[(n1-1):].to_numpy()},
+        index=submitted_entry_orders['date'].iloc[:(1-n1)].to_numpy()
+    ).loc[cancelled_entry_orders['date']]['cancel_date'].to_list()
+    print(cancelled_entry_orders)
+
+    filled_entry_orders = submitted_entry_orders[
+        submitted_entry_orders['trade_id'].isin(
+            list(
+                set(submitted_entry_orders['trade_id']) - set(
+                    cancelled_entry_orders['trade_id']
+                )
+            )
+        )
+    ].copy()
+    filled_entry_orders.reset_index(drop=True, inplace=True)
+    filled_entry_orders['status'] = 'FILLED'
+    for i in range(0, len(filled_entry_orders)):
+
+        idx1 = np.flatnonzero(
+            ivv_prc['Date'] == filled_entry_orders['date'].iloc[i]
+        )[0]
+
+        ivv_slice = ivv_prc.iloc[idx1:(idx1+n1)]['Low Price']
+
+        fill_inds = ivv_slice <= filled_entry_orders['price'].iloc[i]
+
+        if (len(fill_inds) < n1) & (not any(fill_inds)):
+            filled_entry_orders.at[i, 'status'] = 'LIVE'
+        else:
+            filled_entry_orders.at[i, 'date'] = ivv_prc['Date'].iloc[
+                fill_inds.idxmax()
+            ]
+
+    live_entry_orders = pd.DataFrame({
+        "trade_id": ivv_prc.shape[0],
+        "date": pd.to_datetime(next_business_day).date(),
+        "asset": "IVV",
+        "trip": 'ENTER',
+        "action": "BUY",
+        "type": "LMT",
+        "price": round(ivv_prc['Close Price'].iloc[-1] * (1 + alpha1), 2),
+        'status': 'LIVE'
+    },
+        index=[0]
+    )
+
+    if any(filled_entry_orders['status'] =='LIVE'):
+        live_entry_orders = pd.concat([
+            filled_entry_orders[filled_entry_orders['status'] == 'LIVE'],
+            live_entry_orders
+        ])
+        live_entry_orders['date'] = pd.to_datetime(next_business_day).date()
+
+    filled_entry_orders = filled_entry_orders[
+        filled_entry_orders['status'] == 'FILLED'
+        ]
+
+    entry_orders = pd.concat(
+        [
+            submitted_entry_orders,
+            cancelled_entry_orders,
+            filled_entry_orders,
+            live_entry_orders
+        ]
+    ).sort_values(["date", 'trade_id'])
+
+    print("submitted_entry_orders:")
+    print(submitted_entry_orders)
+
+    print("cancelled_entry_orders:")
+    print(cancelled_entry_orders)
+
+    print("filled_entry_orders:")
+    print(filled_entry_orders)
+
+    print("live_entry_orders:")
+    print(live_entry_orders)
+
+    print("entry_orders:")
+    print(entry_orders)
+
+
+
+    print("---------------------------------------------------")
+    # for exit orders
+    ##Parameters:
+    # alpha2 = 0.01
+    # n2 = 5
+
+    ##get inital submitted exit orders:
+    submitted_exit_orders = filled_entry_orders.copy()
+    submitted_exit_orders.reset_index(drop=True, inplace=True)
+    ##update the attribute
+    submitted_exit_orders["trip"] = "EXIT"
+    submitted_exit_orders["action"] = "SELL"
+    submitted_exit_orders["status"] = "SUBMITTED"
+    ##update price
+    submitted_exit_orders["price"] = submitted_exit_orders["price"]*(1 + alpha2)
+    print(submitted_exit_orders)
+    print(ivv_prc)
+    print("---------------------------------------------------------------------")
+    ##get all closed  orders，如果可以fill则fill，如果不能fill并且不够n2天则live
+    print(submitted_exit_orders)
+    exit_orders = submitted_exit_orders.copy()
+    for i in range(0, len(submitted_exit_orders)):
+        idx1 = np.flatnonzero(
+            ivv_prc['Date'] == submitted_exit_orders['date'].iloc[i]
+        )[0]
+
+        ivv_slic = ivv_prc.iloc[idx1:idx1+n2]['High Price']
+
+        fill_inds = ivv_slic >= submitted_exit_orders['price'].iloc[i]
+        # 如果不能fill并且不够n2天则live，修改status
+        if(len(fill_inds) < n2) & (not any(fill_inds)):
+            exit_orders.at[i, 'status'] = 'LIVE'
+        # 如果可以fill则fill，修改price为卖出的price，date为卖出的日期，status为FILLED
+        elif any(fill_inds):
+            # idxt = fill_inds.idxmax()
+            idxt = fill_inds.idxmax()
+            exit_orders.at[i, 'date'] = ivv_prc['Date'].iloc[idxt]
+            exit_orders.at[i, 'price'] = ivv_prc['High Price'].iloc[idxt]
+            exit_orders.at[i, 'status'] = 'FILLED'
+        # 如果不能fill，修改price为close date的price，date为close date，status为CLOSED
+        else:
+            idxt = idx1+n2-1
+            exit_orders.at[i, 'date'] = ivv_prc['Date'].iloc[idxt]
+            exit_orders.at[i, 'price'] = ivv_prc['Close Price'].iloc[idxt]
+            exit_orders.at[i, 'status'] = 'CLOSED'
+    print("final result----------")
+    print(exit_orders)
+
+    return entry_orders, exit_orders
